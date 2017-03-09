e7700cabfffa7e1d482d844affb46524c448ab85
sec <sec@DESKTOP-K14844S>
2017-03-09 11:46:26 +0900

    실습과제1 제출

---
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ *<pre>
+ *org.dimigo.basic
+ *  |_PrimitiveDataType
+ *
+ * 1.개요 : 아이유 프로필
+ * 2.작성일 : 2017. 3. 9.
+ *</pre>
+ *
+ * @author sec
+ * @version : 1.0
+ */
+public class PrimitiveDataType {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("<<아이유 프로필>>");
+		
+		String name = "아이유";
+		boolean isMale = false;
+		int age = 25;
+		double height = 161.8;
+		float weight = (float)44.3;
+		char bloodType = 'A';
+		
+		System.out.println("이름 : "+name);
+		
+		if(isMale)
+			 System.out.printf("성별 : 남자\n");
+		else System.out.printf("성별 : 여자\n");
+		
+		System.out.println("나이 : " +age+"세");
+		System.out.println("키 : "+height+"cm");
+		System.out.println("몸무게 : "+weight+"kg");
+		System.out.println("혈액형 : "+bloodType+"형");
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
